2659d8efb7bd2eaa8fb0a61c5b627991a4b00636
Sleepy105 <lm.sousa@fe.up.pt>
2020-04-28 23:53:04 +0100

    It lives!!!!
    Co-authored-by: Daniel Silva <danny@dannyps.net>
    Co-authored-by: Fábio Gaspar <fabiodanielreis@hotmail.com>

--- src/main.c
+++ src/main.c
@@ -3,42 +3,16 @@
 #include <avr/interrupt.h>
 #include <stdlib.h>
 
-extern uint8_t asmfunction(uint8_t);
-void cfunction(uint8_t);
-uint8_t variable;
+extern uint8_t asmfunction();
+int cfunction(uint8_t, uint32_t);
+uint8_t value = 0;
 
 int main(void)
 {
-    uint8_t value;
-    uint16_t time;
-    variable = 12;
-    value = 0;
-    DDRB = 0xFF;
-    sei(); // enables interrupts globally
-    for (;;)
-    {
-        PORTB = value;
-        value++;
-        for (time = 0; time < 4; time++)
-        {
-            value = value;
-        }
-        value = asmfunction(value);
-        cfunction(value);
-    }
-    cli(); // disables interrupts
+    PORTB = 0x02;
+    asmfunction();
 }
-void cfunction(uint8_t passing)
-{
-    PORTB = passing;
-}
-ISR(SIG_OVERFLOW0) // C handler for timer0
-//overflow interrupt
-// remember that SIGNAL does NOT re-enable
-// interrupts inside the handler
-// If you need a re-entrant handler then
-// use INTERRUPT instead
-{
-    PORTB = 10;
-    TCNT0 = 50;
+
+int cfunction(uint8_t a, uint32_t b) {
+    return PORTB = a + b;
 }
