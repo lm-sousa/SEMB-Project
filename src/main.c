1eda5c018739ceb6b30efeb3f1ffe9acdf7e59f6
Sleepy105 <lm.sousa@fe.up.pt>
2020-05-10 18:23:55 +0100

    Context switch initial commit
    Co-authored-by: Fábio Gaspar <fabiodanielreis@hotmail.com>
    Co-authored-by: Daniel Silva <danny@dannyps.net>

--- src/main.c
+++ src/main.c
@@ -3,162 +3,114 @@
 #include <avr/io.h>
 #include <inttypes.h>
 #include <stdlib.h>
+#include "AVR_CS.h"
 
-#define TASK_BINS 20
 
 #define Hz_10 6250  // compare match register 16MHz/256/10Hz
 #define Hz_2  31250 // compare match register 16MHz/256/2Hz
 #define Hz_2k 31    // compare match register 16MHz/256/2kHz
 
-/* 4 Tasks:
- *     T1 - T = 100ms   -> Led PD3 toggle
- *     T2 - T = 200ms   -> Led PD4 toggle
- *     T3 - T = 600ms   -> Led PD5 toggle
- *     T4 - T = 100ms   -> Led PD6 copied from button A1
- */
+void hardwareInit(int comp){
+    noInterrupts();  // disable all interrupts
+    
+    pinMode(PD3, OUTPUT);
+    pinMode(PD4, OUTPUT);
+    pinMode(PD5, OUTPUT);
+    pinMode(PD6, OUTPUT);
+    pinMode(PD7, OUTPUT);
 
-void t1_init() {
-  pinMode(PD3, OUTPUT);
-}
-void t2_init() {
-  pinMode(PD4, OUTPUT);
-}
-void t3_init() {
-  pinMode(PD5, OUTPUT);
-}
-void t4_init() {
-  pinMode(PD6, OUTPUT);
-}
-void t5_init() {
-  pinMode(PD7, OUTPUT);
+    TCCR1A = 0;
+    TCCR1B = 0;
+    TCNT1 = 0;
+    
+    OCR1A = comp;
+    TCCR1B |= (1 << WGM12);     // CTC mode
+    TCCR1B |= (1 << CS12);      // 256 prescaler
+    TIMSK1 |= (1 << OCIE1A);    // enable timer compare interrupt
+
+    interrupts();  // enable all interrupts
 }
 
-void t1() {
-  digitalWrite(PD3, !digitalRead(PD3));
-}
-void t2() {
-  digitalWrite(PD4, !digitalRead(PD4));
-}
-void t3() {
-  digitalWrite(PD5, !digitalRead(PD5));
-}
-void t4() {
-  digitalWrite(PD6, !digitalRead(PD6));
-}
-void t5() {
-  digitalWrite(PD7, !digitalRead(PD7));
-}
+#define TASK(name, code) void name(void) __attribute__ ((naked)); \
+ void name(void) { \
+    code \
+    asm volatile ( "ret" ); \
+ }
 
-typedef struct {
-  int period;     /* period in ticks */
-  int delay;      /* ticks until next activation */
-  int exec;       /* activation counter */
-  void (*func)(); /* function pointer */
-} Sched_Task_t;
-
-Sched_Task_t Task_List[TASK_BINS];
-
-/**
- * @brief Initialise data structures.
- * Configure interrupt that periodically calls Sched_Schedule().
- *
- */
-void Sched_Init(int comp) {
-  for (int x = 0; x < TASK_BINS; x++) {
-    Task_List[x].func = 0;
-  }
-
-  /* Also configures interrupt that periodically calls Sched_Schedule(). */
-  noInterrupts();  // disable all interrupts
-
-  TCCR1A = 0;
-  TCCR1B = 0;
-  TCNT1 = 0;
- 
-  OCR1A = comp;
-  TCCR1B |= (1 << WGM12); // CTC mode
-  TCCR1B |= (1 << CS12); // 256 prescaler
-  TIMSK1 |= (1 << OCIE1A); // enable timer compare interrupt
-
-  interrupts();  // enable all interrupts
-}
-
-int Sched_AddT(void (*f)(), int d, int p) {
-  for (int x = 0; x < TASK_BINS; x++) {
-    if (!Task_List[x].func) {
-      Task_List[x].period = p;
-      Task_List[x].delay = d;
-      Task_List[x].exec = 0;
-      Task_List[x].func = f;
-      return x;
+TASK(t1, {
+    for(int i = 0; i < 10000; i++) {
+        asm("nop");
     }
-  }
+    digitalWrite(PD3, !digitalRead(PD3));
+});
 
-  return -1;
+void runTask(void (*f)()) {
+    while (true) {
+        f();
+    }
 }
 
-/**
- * @brief Verifies if any task needs to be activated, and if so, increments by 1
- * the task's pending activation counter.
- *
- */
-void Sched_Schedule() {
-  for (int x = 0; x < TASK_BINS; x++) {
-    if (!Task_List[x].func) {  // deleted tasks have func set to NULL
-      continue;
-    }
+void* example = NULL;
 
-    if (Task_List[x].delay) {
-      Task_List[x].delay--;
-    } else {
-      /* Schedule Task */
-      Task_List[x].exec++;
-      Task_List[x].delay = Task_List[x].period - 1;
-    }
-  }
+void addTask(void (*f)()) {
+    example = f;
 }
 
-/**
- * @brief Verifies if any task has an activation counter > 0, and if so, calls
- * that task.
- *
- */
-void Sched_Dispatch() {
-  for (int x = 0; x < TASK_BINS; x++) {
-    if (Task_List[x].exec) {
-      Task_List[x].exec--;
-      Task_List[x].func();
-      /* Delete task if one-shot */
-      if (!Task_List[x].period) {
-        Task_List[x].func = NULL;
-      }
-    }
-  }
+void vPortYieldFromTick( void ) __attribute__ ( ( naked ) );
+void vTaskIncrementTick( void );
+void vTaskSwitchContext( void );
+
+volatile TCB_t * volatile pxCurrentTCB;
+
+void vPortYieldFromTick( void ) {
+    /* This is a naked function so the context
+    is saved. */
+    portSAVE_CONTEXT();
+
+    /* Increment the tick count and check to see
+    if the new tick value has caused a delay
+    period to expire. This function call can
+    cause a task to become ready to run. */
+    vTaskIncrementTick();
+    
+    /* See if a context switch is required.
+    Switch to the context of a task made ready
+    to run by vTaskIncrementTick() if it has a
+    priority higher than the interrupted task. */
+    vTaskSwitchContext();
+    
+    /* Restore the context. If a context switch
+    has occurred this will restore the context of
+    the task being resumed. */
+    portRESTORE_CONTEXT();
+    
+    /* Return from this naked function. */
+    asm volatile ( "ret" );
 }
 
-volatile int go = 0;
 
-ISR(TIMER1_COMPA_vect) {
-  t4();
-  Sched_Schedule();
+void vTaskIncrementTick() {
+    // TODO
+    return;
+}
+
+void vTaskSwitchContext() {
+    // TODO
+    return;
+}
+
+ISR(TIMER1_COMPA_vect, ISR_NAKED) {
+    /* Call the tick function. */
+    vPortYieldFromTick();
+
+    /* Return from the interrupt. If a context
+    switch has occurred this will return to a
+    different task. */
+    asm volatile ( "reti" );
 }
 
 int main() {
-  // initialize the kernel
-  Sched_Init(Hz_2);
-  t1_init();
-  t2_init();
-  t3_init();
-  t4_init();
-  t5_init();
-  /* periodic task */
-  Sched_AddT(t1, 2, 4);
-  Sched_AddT(t2, 2, 8);
-  Sched_AddT(t5, 0, 1);
-  /* one-shot task */
-  Sched_AddT(t3, 50, 0);
-
-  while (true) {
-    Sched_Dispatch();
-  }
+    hardwareInit(Hz_2);
+    addTask(t1);
+    runTask(example);
 }
