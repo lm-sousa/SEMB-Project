beb6f01dd7a6466cf2b87bebf72fcd8b7d0e29b6
Sleepy105 <lm.sousa@fe.up.pt>
2020-06-08 23:30:58 +0100

    Ready as-is

--- src/main.c
+++ src/main.c
@@ -3,7 +3,7 @@
 /*****************************************************************************/
 /******************************* CONFIGURATION *******************************/
 /*****************************************************************************/
-#define TICK_FREQUENCY      Hz_100
+#define TICK_FREQUENCY      Hz_20
 #define STACK_SIZE_DEFAULT  100
 #define MAX_TASKS           3
 #define NUMBER_OF_MUTEXES   8
@@ -16,17 +16,19 @@
 
 /**************************** ADD YOUR TASKS HERE ****************************/
 
-TASK(t1, 1, Hz_1, {
-    PORTD ^= _BV(2);    // Toggle
+TASK(t1, 4, Hz_2, 0, {
+    //if(trylock(1))
+        PORTD ^= _BV(2);    // Toggle
     suspend();
 });
 
-TASK(t2, 4, Hz_1, 2000, {
-    PORTD ^= _BV(3);    // Toggle
+TASK(t2, 4, Hz_2, 1000, {
+    //if (trylock(1))
+        PORTD ^= _BV(3);    // Toggle
     suspend();
 });
 
-TASK(t3, 5, Hz_2, 0, {
+TASK(t3, 4, Hz_2, 5000, {
     PORTD ^= _BV(4);    // Toggle
     suspend();
 });
